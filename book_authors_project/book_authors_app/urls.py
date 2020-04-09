54955944f6cc9de1a08b2633e5013122081e20c8
Connor <connorskorburg@gmail.com>
2020-04-09 12:06:24 -0500

    added authors page to book authors project

--- book_authors_project/book_authors_app/urls.py
+++ book_authors_project/book_authors_app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index),
     path('add_book', views.add_book),
     path('books/<int:number>', views.books),
+    path('add_author_to_book', views.add_author_to_book),
+    path('author', views.author),
     path('add_author', views.add_author),
+    path('author/<int:number2>', views.view_author),
+    path('add_book_to_author', views.add_book_to_author),
 ]
--- book_authors_project/book_authors_app/views.py
+++ book_authors_project/book_authors_app/views.py
@@ -26,13 +26,47 @@ def books(request,number):
     }
     return render(request, 'books.html', context)
 
-def add_author(request):
+def add_author_to_book(request):
     # print("book ID:", request.session['number'])
-    # print("author ID:", request.POST['author'])
+    # print("author ID:", request.POST['author_1'])
     book_id = request.session['number']
-    author_id = int(request.POST['author'])
+    author_id = int(request.POST['author_1'])
     new_author = Author.objects.get(id=author_id)
     info = Book.objects.get(id=book_id)
     info.authors.add(new_author)
-    # print(info.authors.all())
     return redirect('/')
+
+def author(request):
+    data = Author.objects.all()
+    context = {
+        'all_authors': data, 
+    }
+    return render(request, 'authors.html', context)
+
+def add_author(request):
+    print(request)
+    print(request.POST)
+    Author.objects.create(first_name=request.POST["first_name"], last_name=request.POST["last_name"], notes=request.POST["notes"])
+    return redirect('/author')
+
+def view_author(request, number2):
+    request.session['number2'] = number2
+    data = Author.objects.get(id=request.session['number2'])
+    data_books = data.books.all()
+    data_all_books = Book.objects.all()
+    context = {
+        'author_view': data,
+        'current_books': data_books,
+        'all_author_books': data_all_books,
+    }
+    return render(request, 'view_authors.html', context)
+
+def add_book_to_author(request):
+    # print("author ID:", request.session['number2'])
+    # print("book ID:", request.POST['book_added'])
+    author_id_ = request.session['number2']
+    book_added_id = int(request.POST['book_added'])
+    new_book = Book.objects.get(id=book_added_id)
+    data = Author.objects.get(id=author_id_)
+    data.books.add(new_book)
+    return redirect('/author')
